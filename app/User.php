0c345a77aab97f9064300a51a4e13b3126909067
Victor Nuñez <skeiceee@gmail.com>
2018-12-10 16:25:39 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace genericlothing;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use DB;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -32,4 +32,28 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    public function getCodPedido($rut_cliente){
+      return DB::table('pedido')
+             ->select('cod_pedido')
+             ->where('rut_cliente', '=', DB::raw('\''.$rut_cliente.'\''))->value('cod_pedido');
+    }
+
+    public function detallePedidoEmpty($rut_cliente){
+      $boolean = TRUE;
+
+      $cod_pedido = DB::table('pedido')
+             ->select('cod_pedido')
+             ->where('rut_cliente', '=', DB::raw('\''.$rut_cliente.'\''))->value('cod_pedido');
+
+      $cant = DB::table('detalle-pedido')
+                    ->select(DB::raw('count(*) as cant'))
+                    ->where('cod_pedido', '=', DB::raw('\''.$cod_pedido.'\''))->value('cant');
+
+      if ($cant == 0) {
+        return true;
+      }else {
+        return false;
+      }
+    }
 }
